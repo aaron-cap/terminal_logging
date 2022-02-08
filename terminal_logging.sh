fadbe3212054cc9a9a3af404a6245280336a34a1
Aaron <a.diep95@gmail.com>
2022-02-08 14:56:23 +0100

    Update terminal_logging.sh

--- terminal_logging.sh
+++ terminal_logging.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+###############################################
 # Author: Aaron van Diepen
 # Append a line in /etc/bash.bashrc to:
 # Automatically log all terminal/SSH sessions
+###############################################
 
+# write comment to file
 echo "# Automatically log all terminal/SSH sessions" >> /etc/bash.bashrc
+# write command to file
 echo "test \"\$(ps -ocommand= -p $PPID | awk '{print \$1}')\" == 'script' || (script -f $HOME/logs/\$(date +\"%d-%b-%y_%H-%M-%S\").\${HOSTNAME:-\$(hostname)}.\$\$.\${RANDOM}_shell.log)" >> /etc/bash.bashrc
